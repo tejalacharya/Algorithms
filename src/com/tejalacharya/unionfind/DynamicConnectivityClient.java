5b48e66f0364b7ca97d9699fb0fd2b6c2fe1e19a
Tejal Acharya <tejal.acharya@gmail.com>
2015-06-19 15:09:26 -0400

    First commit

---
+++ src/com/tejalacharya/unionfind/DynamicConnectivityClient.java
@@ -0,0 +1,18 @@
+package com.tejalacharya.unionfind;
+
+import java.util.Scanner;
+
+/**
+ * Created by tacharya on 6/19/2015.
+ */
+public class DynamicConnectivityClient {
+    public static void main(String[] args){
+        Scanner scan =  new Scanner(System.in);
+        int a = scan.nextInt();
+
+        while(scan != null){
+            int p = scan.nextInt();
+            int q = scan.nextInt();
+        }
+    }
+}
---
+++ src/com/tejalacharya/unionfind/UF.java
@@ -0,0 +1,12 @@
+package com.tejalacharya.unionfind;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/**
+ * Created by tacharya on 6/19/2015.
+ */
+public class UF {
+
+
+}
